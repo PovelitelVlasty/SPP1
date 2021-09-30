4d069459daa5b8c9fc6deebbc22e43465b3d9546
PovelitelVlasty <79055161+PovelitelVlasty@users.noreply.github.com>
2021-09-30 21:31:45 +0300

    5/3

---
+++ ConsoleApplication/Writer/FileWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace ConsoleApplication.Writer
+{
+    public class FileWriter
+    {
+        public void Write(string result, string filepath)
+        {
+            using StreamWriter writer = new StreamWriter(filepath);
+            writer.Write(result);
+        }
+    }
+}
